f356df5799a784ea93291d4fabe2e718cf342302
lenhat41555 <70925729+lenhat41555@users.noreply.github.com>
2020-09-22 11:10:02 +0700

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Studenst
+{
+    class Program
+    {
+      
+
+        private static void Main1(string[] args)
+        {
+            Student student1 = new Student(1, "le nhat", "hue", 30);
+
+            Student student2 = new Student();
+            student2.SetId(2);
+            student2.SetName("Ph. Quang Khánh");
+            student2.SetAddress("Lạng Sơn");
+            student2.SetAge(25);
+
+            Console.WriteLine(student1.ToString());
+            Console.WriteLine(student2.ToString());
+            Console.Read();
+        }
+    }
+}
